ac853d4414de018bee3f3dc58bca238845d2bf9b
Andrew0620 <andrew0620@msn.com>
2018-08-10 01:50:39 +0800

    add JavaScript

---
+++ iPet/main.js
@@ -0,0 +1,7 @@
+// 事件處理機制, 就線監聽 button, 當點擊時, 需做什麼事情 //
+
+var button = document.getElementById('abbutton');
+
+butotn.addEventListener('click', function(){
+    alert('click me!!!')
+});      // 點擊時, 會出現 click me!!!//
