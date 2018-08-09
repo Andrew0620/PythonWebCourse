07e2b36d04d3f188f926ea5d9e44e468943f98cc
Andrew0620 <andrew0620@msn.com>
2018-08-10 00:39:07 +0800

    Building a module

--- ModuleHero/index.py
+++ ModuleHero/index.py
@@ -1,9 +1,17 @@
-# 建立自己的模組
+### 建立自己的 module 和 package, 可讓程式更加模組化, 方便使用
 '''
-import hello
 hello.print_helloworld()
 '''
-
+'''
 from hello import print_helloworld as phw
 
 phw()
+'''
+
+## 用 package 需先新增一個空白的 __init__.py 的文件, 讓系統知了面放了 module
+
+from mypackage.awesome import print_awesome
+from mypackage.happy import print_happy
+
+print_awesome()
+print_happy()
