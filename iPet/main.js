0858f6cbc62c8ac06b8389045383b0c806cca1ff
Andrew0620 <andrew0620@msn.com>
2018-08-10 02:07:48 +0800

    載入公開資料

--- iPet/main.js
+++ iPet/main.js
@@ -1,7 +1,15 @@
 // 事件處理機制, 就線監聽 button, 當點擊時, 需做什麼事情 //
 
 var button = document.getElementById('abbutton');
+var result = document.getElementById('result');
 
-button.addEventListener('click', function(){
-    alert('click me!!!');
-});      // 點擊時, 會出現 click me!!!//
+button.addEventListener('click', function() {
+    var xhr = new XMLHttpRequest();
+    var handleOnload = function() {
+        console.log(xhr.responseText);
+    }
+
+    xhr.open('GET', 'http://163.29.157.32:8080/dataset/6a3e862a-e1cb-4e44-b989-d35609559463/resource/f4a75ba9-7721-4363-884d-c3820b0b917c/download/393625397fc043188a3f8237c1da1c6f.json,true')
+    xhr.send();
+    xhr.onload = handleOnload;
+});      // 點擊時, 會出現 {} 的設定, true 表非同步處理//
