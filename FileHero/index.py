35fdd1fcc2bd9120a7b48566da7edeb9859f8a13
Andrew0620 <andrew0620@msn.com>
2018-08-09 22:40:52 +0800

    FileHero

---
+++ FileHero/index.py
@@ -0,0 +1,11 @@
+import os
+
+filename = 'hello_word.txt'
+
+if os.path.exists(filename)
+    os.remove(filename)  #如果有 hello_world.txt 這個資料, 就移進去
+
+else:
+    with open(filename, 'w') as f:    #沒有就新增檔案, 用 with; w=write, r=read, a=append
+        f.write('Hello World!!!')
+
