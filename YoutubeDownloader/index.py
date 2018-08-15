2e064bc8ce91b5ed3da59bbb19d3c806164a9d10
Andrew0620 <andrew0620@msn.com>
2018-08-16 00:15:38 +0800

    YoutubrDownloader html

--- YoutubeDownloader/index.py
+++ YoutubeDownloader/index.py
@@ -1,1 +1,10 @@
-print('Hello Youtube')
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=Ture)
