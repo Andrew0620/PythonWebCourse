f47a8c8bb1e751ec4f558677e6516b6449f4d386
Andrew0620 <andrew0620@msn.com>
2018-08-16 00:50:49 +0800

    YoutubrDownloader

--- YoutubeDownloader/index.py
+++ YoutubeDownloader/index.py
@@ -1,11 +1,23 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request, redirect
+from pytube import YouTube
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def post_submit():
+    yt = YouTube()
+    url = request.form.get('url')
+    yt.url = url
+    video = yt.get('mp4', '360p')
+    video.download('./')
+    filename = yt.filename
+    print(yt)
+    print(yt.filname)
+    return redirect(url_for('index', filename=filename)) #回跳到首頁
 
 if __name__ == '__main__':
     app.run(debug=True)
