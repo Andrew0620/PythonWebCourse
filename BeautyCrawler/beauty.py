1567f41c5ebdcec1989e66d0b993780358ca5554
Andrew0620 <andrew0620@msn.com>
2018-08-26 15:05:37 +0800

    BesutyCrawler

---
+++ BeautyCrawler/beauty.py
@@ -0,0 +1,9 @@
+import request
+
+headers = {
+	'user-agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36'
+}   # 讓網路爬蟲像真人的一個模式, 因為有些網頁管理員會擋    # Mozilla 是瀏覽器的一個內核
+
+res = request.get('https://www.ptt.cc/bbs/Beauty/M.1535220756.A.4C0.html', headers=headers)  # get & post 差異
+
+print(res.text)
