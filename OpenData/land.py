58090dfcc9f6855dfc784b0237e1e8ce3a27cfcc
Andrew0620 <andrew0620@msn.com>
2018-08-30 21:31:36 +0800

    Opendata

--- OpenData/land.py
+++ OpenData/land.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-res = pd.read_csv('./lvr_landAcsv_201703/A_LVR_LAND_C.CSV')
+res = pd.read_csv('./lvr_landAcsv_201703/A_LVR_LAND_C.CSV', encoding='big5')
 
 print(res.describe())
 print(res.mean())
