6468aa17bdacf6e74eac79b8536d476f8b37d684
Andrew0620 <andrew0620@msn.com>
2018-08-30 20:52:07 +0800

    Opendata

--- Opendata/opendata.py
+++ Opendata/opendata.py
@@ -1,3 +1,5 @@
+# 結合開放資料去做一些簡單的資料分析應用
+
 import requests
 import json
 import matplotlib.pyplot as plt
@@ -6,6 +8,21 @@ url = 'http://data.fda.gov.tw/cacheData/35_3.json'
 
 res= requests.get(url)
 
-#print(res.text)
+# print(res.text)
 items =json.loads(res.text)  # 把 json 檔引入進來, 變成 python 可以操作的一個方法
+
+data = [0, 0]
+
+for item in items:
+	# print(item)
+	if item[6]['負責人性別'] == '男':
+		data[0] += 1
+	elif item[6]['負責人性別'] == '女':
+		data[1] += 1
+labels = ['man', 'woman']
+
+plt.pie(data, labels=labels)
+plt.show()
+
+
 
