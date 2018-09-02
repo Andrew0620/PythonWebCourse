4991e87353b1b45f6c0014dd33239aa9c61d25fe
Andrew0620 <andrew0620@msn.com>
2018-09-03 01:26:06 +0800

    Opendata

--- HowOldAreYou/index.py
+++ HowOldAreYou/index.py
@@ -1,11 +1,26 @@
 import cv2
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
+@app.route('/upload', methods=['POST'])  # 處理網址列的部分
+def upload():
+    if request.method == 'POST':
+        file = request.files['img']
+
+        file.save('./' + file.filename)
+
+        cascPath = 'haarcascade_frontalface_default.xml'
+
+        faceCascade =cv2.CascadeClassifier(cascPath)  # 把特徵值吃進來
+
+        image = cv2.imread(file.filename)   # 把 img 吃進來, imr= read image
+        gray = cd2.cvtColor(image, cv2.COLOR_BAYER_BG2GRAY)  # 把圖片從 BGR 轉成 gray
+
+        return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
