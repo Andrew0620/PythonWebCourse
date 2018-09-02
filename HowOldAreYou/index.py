7feb03871b6b0121da34e9fc871a4ff7fceb746f
Andrew0620 <andrew0620@msn.com>
2018-09-03 01:55:11 +0800

    人臉辨識

--- HowOldAreYou/index.py
+++ HowOldAreYou/index.py
@@ -17,8 +17,8 @@ def upload():
 
         faceCascade =cv2.CascadeClassifier(cascPath)  # 把特徵值吃進來
 
-        image = cv2.imread(file.filename)   # 把 img 吃進來, imr= read image
-        gray = cd2.cvtColor(image, cv2.COLOR_BAYER_BG2GRAY)  # 把圖片從 BGR 轉成 gray
+        image = cv2.imread(file.filename)   # 把 img 吃進來, imr=read image
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)  # 把圖片從 BGR 轉成 gray
 
         return render_template('index.html')
 
