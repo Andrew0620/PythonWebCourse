31179875cd2d6efb60eabd8e397b02b19b3d29cd
Andrew0620 <andrew0620@msn.com>
2018-09-05 22:51:34 +0800

    人臉辨識第三階段, 系統優化, 直接顯示在網頁上

--- HowOldAreYou/index.py
+++ HowOldAreYou/index.py
@@ -1,12 +1,14 @@
 import cv2
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 import random
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html')
+    filename = request.args.get('filename')
+    result = request.args.get('result')
+    return render_template('index.html', filename = filename, result = result)
 
 @app.route('/upload', methods=['POST'])  # 處理網址列的部分
 def upload():
@@ -37,7 +39,8 @@ def upload():
 
         for (x, y, w, h) in faces:
             age = random.randint(18, 30)  # 年齡設定範圍
-            cv2.rectangle(image, (x + 3, y - 15), (x + 50, y - 50), (200, 187, -1), -1) # 放年齡文字的地方
+            cv2.rectangle(image, (x + 3, y - 15), (x + 50, y - 50), (200, 187, -1), -1) 　
+            # 放年齡文字的地方
             cv2.putText(image, str(age), (x + 5, y - 20), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0, 187), 2)
             # 塞進年齡, cv2.FONT_HERSHEY_SIMPLEX 字體形式
             cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
@@ -47,6 +50,10 @@ def upload():
         # （0,255,0）是画线对应的 rgb 颜色, 2 是所画的线的宽度(-1表實心)
         #  cv2.imwrite: 第一个参数是文件名，第二个参数是你要保存的图片，
         # 第三个参数针对特定的格式： 对于JPEG，其表示的是图像的质量，用0-100的整数表示，默认为95
+        newImage = './static/images/' + file.filename
+
+        return redirect(url_for('index', filename = newImage, result = len(faces)))
+        # 重新定向到 index 的地方, 帶出檔名, result 看是否辨識出人臉: 0 代表 fail
 
         return render_template('index.html')
 
