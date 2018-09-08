5d4d0a1d265c6e2492561d126de52f2abd8f84e6
Andrew0620 <andrew0620@msn.com>
2018-09-08 17:31:43 +0800

    OpenCook

--- OpenCook/opencook/opencook/urls.py
+++ OpenCook/opencook/opencook/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.conf.urls import url
 
 from mainapp.views import get_index
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'admin/', admin.site.urls),
     url(r'^', get_index)   # 進到首頁讓 get_index 去處理
 ]
