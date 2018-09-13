7c41c1ce9505aa2fb041c4bebc4ec71fd5c03415
Andrew0620 <andrew0620@msn.com>
2018-09-14 02:08:50 +0800

    單元 32 網站球後端分離與 restful API
    怎樣由 python django 去提供 API讓前端來進行組織資料, 最後進行前後端的分離機制

--- OpenCook/opencook/opencook/urls.py
+++ OpenCook/opencook/opencook/urls.py
@@ -25,14 +25,16 @@ from django.conf.urls import url
 
 from mainapp.views import get_index, get_signup, post_signup, post_login, post_logout
 
+from recipe.views import get_recipes_api # 開一個 API 接口
+
 urlpatterns = [
-    url(r'admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
     url(r'^signup$', get_signup),  # ^ 表字首對應; $ 表字尾對應
     url(r'^signup/post$', post_signup),
-    url(r'^login/post$', post_logout),
+    url(r'^login/post$', post_login),
     url(r'^logout/post$', post_logout),
+    url(r'^api/recipes$', get_recipes_api),   # 定義一個 API 接口, 會吐回所有的 recipes 的列表
     url(r'^', get_index)   # 進到首頁, url 網址會交由 get_index 這個 views 去處理
-
 ]
 '''
 urlpatterns = [
--- OpenCook/opencook/recipe/views.py
+++ OpenCook/opencook/recipe/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+# API 接口
 
+from django.shortcuts import render
+from .models import Recipe
+from django.http import JsonResponse   # 引入 json response 的一個套件, 可以幫助我們簡化
+from django.core import serializers
 # Create your views here.
+
+def get_recipes_api(request):
+    recipes = Recipe.objects.all()
+    data = serializers.serialize('json', recipes)# recipe 取出來事實上是 query set, 要進行資料轉換轉成 json 的 format, json format 是一種輕量式傳遞資料的方式
+    return JsonResponse({'data': data})
+
+'''
+介紹 html, css and javascript 的時候, 有使用一個 ajax 的一個方法, 可透過 ajax 的方法, 在前端使用 javascript 去呼叫 API, 那我們 return 的 json 的 data 就可以在前端進行組件我們前端的介面, 把資料榮進 html 裡面
+'''
